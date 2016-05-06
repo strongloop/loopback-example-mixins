f6253446bc412ea393ea4f28141f83b8f4025553
Ryan Graham <r.m.graham@gmail.com>
2016-05-06 16:28:42 -0700

    update copyright notices and license

--- test/common.js
+++ test/common.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-example-mixins
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 /* exported global */
 global.loopback = require('loopback');
 global.assert = require('assert');
